a6f27ab382a406a005efd937593869cf896ae62a
Gagandeep Sharma <gagandsharma90@gmail.com>
2021-06-20 12:04:47 +0530

    Course Scheduling Problem 2

--- src/main/java/com/dsa/graph/CourseScheduleProblem.java
+++ src/main/java/com/dsa/graph/CourseScheduleProblem.java
@@ -7,7 +7,7 @@ public class CourseScheduleProblem {
 
     public static void main(String[] args) {
         int numCourses = 2;
-        int[][] prerequisites = {{1,0},{0,1}};
+        int[][] prerequisites = {{1, 0}, {0, 1}};
 
         CourseScheduleProblem schedule = new CourseScheduleProblem();
         boolean canFinish = schedule.canFinish(numCourses, prerequisites);
@@ -20,31 +20,31 @@ public class CourseScheduleProblem {
         int[] dependentCoursesCount = new int[numCourses];
 
         // Create a Course dependency graph
-        for(int i = 0; i < prerequisites.length;i++) {
+        for (int i = 0; i < prerequisites.length; i++) {
 
             int courseToDo = prerequisites[i][0]; // Course user is trying to do
             int dependentCourse = prerequisites[i][1]; // dependent course needed to complete first
 
-            if(courseConnections[dependentCourse][courseToDo] == 0)
+            if (courseConnections[dependentCourse][courseToDo] == 0)
                 dependentCoursesCount[courseToDo]++;
             courseConnections[dependentCourse][courseToDo] = 1;
         }
 
         // Add Courses to queue which have no dependencies
         Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < dependentCoursesCount.length;i++) {
-            if(dependentCoursesCount[i] == 0)
+        for (int i = 0; i < dependentCoursesCount.length; i++) {
+            if (dependentCoursesCount[i] == 0)
                 queue.offer(i);
         }
 
         int count = 0;
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int course = queue.poll();
             count++;
 
-            for(int i = 0; i < numCourses; i++) {
-                if(courseConnections[course][i] != 0)
-                    if(--dependentCoursesCount[i] == 0)
+            for (int i = 0; i < numCourses; i++) {
+                if (courseConnections[course][i] != 0)
+                    if (--dependentCoursesCount[i] == 0)
                         queue.offer(i);
             }
         }
